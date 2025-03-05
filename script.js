ca0d9288e8ddf1f9664ef4e41ce6bdf32e74db6f
amauri-castro <amauri.p.castro@gmail.com>
2025-03-05 09:54:43 -0300

    alterado script.js

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function fazerRequisicao() {
             document.getElementById("resposta").textContent = 'Erro: ' + error.message;
         });
 }
+
+function fazerRequisicaoPut() {
+
+    const url = "http://localhost:8080/restaurantes/1/fechamento";
+
+    fetch(url, {
+        method: "PUT"})
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("resposta").textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            document.getElementById("resposta").textContent = 'Erro: ' + error.message;
+        });
+}
