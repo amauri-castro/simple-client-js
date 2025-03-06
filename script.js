8264b2c6905a434f9cd99211ccae6008555f7a93
amauri-castro <amauri.p.castro@gmail.com>
2025-03-05 21:55:52 -0300

    Implementado mais metodos para consulta a api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function fazerRequisicao() {
+const url = "http://localhost:8080/formas-pagamento";
 
-    const url = "http://localhost:8080/formas-pagamento";
+function fazerRequisicao() {
 
     fetch(url, {headers: { "Content-Type": "application/json"}})
         .then(response => {
@@ -19,6 +19,42 @@ function fazerRequisicao() {
         });
 }
 
+function salvarDados() {
+    let inputDescricao = document.getElementById("campoDescricao");
+
+    const formaPagamento = JSON.stringify({
+        "descricao": inputDescricao.value
+    })
+    console.log(inputDescricao)
+
+    fetch(url, {
+        headers: { "Content-Type": "application/json"},
+        method: 'post',
+        body: formaPagamento,
+    }).then(() => {
+        fazerRequisicao()
+        inputDescricao.value = '';
+    } )
+    .catch(error => {
+        const elementoErro = document.getElementById("erro");
+        elementoErro.textContent = 'Erro: ' + error.message;
+        elementoErro.style.display = "inline-block"
+    });
+}
+
+function excluir(idItem) {
+    console.log(idItem)
+    fetch(url + `/${idItem}`, {
+        method: "DELETE"
+    }).then(() => {
+        fazerRequisicao();
+    }).catch(error => {
+        const elementoErro = document.getElementById("erro");
+        elementoErro.textContent = 'Erro: ' + error.message;
+        elementoErro.style.display = "inline-block"
+    });
+}
+
 function preencherTabela(dados) {
     const tabela = document.getElementById("tabela");
     tabela.innerHTML = "";
@@ -29,6 +65,7 @@ function preencherTabela(dados) {
         linha.innerHTML = `
             <td>${item.id}</td>
             <td>${item.descricao}</td>
+            <td><button onclick="excluir(${item.id})">Delete</button></td>
         `;
 
         tabela.appendChild(linha);
